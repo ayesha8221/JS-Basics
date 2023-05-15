1a48a0c1eef18e5de29cc896e96d47f5462ab425
Ayesha <ayesha.glnt012@gmail.com>
2023-05-15 20:50:41 +0200

    day2 coding exercises

---
+++ index.js
@@ -0,0 +1,19 @@
+function myFunction() {
+    alert ("Hello! My name is Ayesha!!");
+}
+
+ // This is my Javascript statement
+ console.log('Hello World');
+
+//  Day2 Coding Exercises
+ var name="Ayesha";
+ var surname= "galant";
+ console.log(name+surname);
+
+ var numberVar= 5;
+ console.log(numberVar*5);
+
+ const myName= "Ayesha"
+ const myName= "Galant"
+
+
