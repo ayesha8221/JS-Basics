1abfaf9a73d32bf591fb3e341b61de56d39d1067
Ayesha <ayesha.glnt012@gmail.com>
2023-05-15 23:00:51 +0200

    day3 coding exercises

--- index.js
+++ index.js
@@ -13,7 +13,34 @@ function myFunction() {
  var numberVar= 5;
  console.log(numberVar*5);
 
- const myName= "Ayesha"
- const myName= "Galant"
+ const myName= "Ayesha";
+ const myName= "Galant";
 
+//  Day3 Coding Exercises
+
+//  string data type variable 
+// is anything with single or double quotes
+
+var name= "Ayesha is a Galant";
+
+// numbers data type
+// any number, neg or pos
+
+var number= 25363253373;
+
+// Boolean 1=true, 0=false
+ var bool1=true;
+ var bool0= false;
+
+//  Undefined 
+// anything with no value added will return undefined
+
+var myAge= ;
+
+// Null
+var myAge1= null;
+
+// Check Data Types
+
+console.log(typeof myAge1);
 
